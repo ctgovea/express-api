3745caa71653147823c8973c5f29e6f65ea27aaf
Carlos Govea <cgovea@hagerty.com>
2023-06-27 20:19:20 -0600

    fix: contract by id

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const bodyParser = require('body-parser')
+const { Op } = require('sequelize')
 const { sequelize } = require('./model')
 const { getProfile } = require('./middleware/getProfile')
 const app = express()
@@ -8,13 +9,18 @@ app.set('sequelize', sequelize)
 app.set('models', sequelize.models)
 
 /**
- * FIX ME!
  * @returns contract by id
  */
 app.get('/contracts/:id', getProfile, async (req, res) => {
   const { Contract } = req.app.get('models')
   const { id } = req.params
-  const contract = await Contract.findOne({ where: { id } })
+  const { profile } = req
+  const contract = await Contract.findOne({
+    where: {
+      [Op.and]: [{ id }],
+      [Op.or]: [{ ContractorId: profile.id }, { ClientId: profile.id }]
+    }
+  })
   if (!contract) return res.status(404).end()
   res.json(contract)
 })
